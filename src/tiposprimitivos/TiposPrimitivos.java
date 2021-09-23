58febf8e660bd49c49975a69cf3f07cb6c121983
Gemamm <91058451+Gemamm@users.noreply.github.com>
2021-09-23 10:15:02 +0200

    Initial commit

---
+++ src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author gemam
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte numByte = 5;
+        //byte numByte = {byte}129;
+        System.out.println("numero byte: "+numByte);
+        System.out.println("Valor mínimo del tipo byte es: "+Byte.MIN_VALUE );
+        System.out.println("Valor máximo del tipo byte es: "+Byte.MAX_VALUE );
+    }
+    
+}
